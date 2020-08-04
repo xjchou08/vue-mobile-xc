fda9a86e13a6add6b607f6aa3a4dadf885b71857
81zhouxiaojia <1207638676@qq.com>
2020-08-04 16:18:00 +0800

    上传新的代码

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,10 +11,10 @@
       <!-- banner end -->
       <div class="local-nav">
         <li>
-          <a href="javascript:;"><span>攻略.景点</span></a>
+          <a href="javascript:;"><span>攻略&middot;景点</span></a>
         </li>
         <li>
-          <a href="javascript:;"><span>门票.玩乐</span></a>
+          <a href="javascript:;"><span>门票&middot;玩乐</span></a>
         </li>
         <li>
           <a href="javascript:;"><span>美食林</span></a>
@@ -30,15 +30,15 @@
       <nav>
         <div class="nav-common">
           <div class="nav-item item1"><span>酒店</span></div>
-          <div class="nav-item item2"><span>民宿.客栈</span></div>
-          <div class="nav-item item3"><span>特价.爆款</span></div>
+          <div class="nav-item item2"><span>民宿&middot;客栈</span></div>
+          <div class="nav-item item3"><span>特价&middot;爆款</span></div>
         </div>
 
         <div class="nav-common">
           <div class="nav-item item4"><span>机票</span></div>
           <div class="nav-item item5"><span>火车票</span></div>
-          <div class="nav-item item6"><span>汽车.船票</span></div>
-          <div class="nav-item item7"><span>专车.租车</span></div>
+          <div class="nav-item item6"><span>汽车&middot;船票</span></div>
+          <div class="nav-item item7"><span>专车&middot;租车</span></div>
         </div>
 
         <div class="nav-common">
@@ -60,12 +60,12 @@
         </li>
         <li>
           <a href="javascript:;"
-            ><span class="main-nav-icon3">保险.签证</span></a
+            ><span class="main-nav-icon3">保险&middot;签证</span></a
           >
         </li>
         <li>
           <a href="javascript:;"
-            ><span class="main-nav-icon4">换钞.签证</span></a
+            ><span class="main-nav-icon4">换钞&middot;签证</span></a
           >
         </li>
         <li>
@@ -83,21 +83,21 @@
         </li>
         <li>
           <a href="javascript:;"
-            ><span class="main-nav-icon8">电卡.借钱</span></a
+            ><span class="main-nav-icon8">电卡&middot;借钱</span></a
           >
         </li>
         <li>
-          <a href="javascript:;"><span class="main-nav-icon9">旅拍</span></a>
+          <a href="javascript:;"><span class="main-nav-icon9">旅 拍</span></a>
         </li>
         <li>
-          <a href="javascript:;"><span class="main-nav-icon10">更多</span></a>
+          <a href="javascript:;"><span class="main-nav-icon10">更 多</span></a>
         </li>
       </ul>
       <!-- 自由行 end -->
       <div class="hotAdvise">
         <h2></h2>
         <i class="advise"></i>
-        <a href="javascript:;"><span>获取更多福利 ></span></a>
+        <a href="javascript:;"><span>获取更多福利</span></a>
       </div>
       <!-- 热门活动 end-->
       <div class="row-adv">
@@ -114,7 +114,7 @@
             ><i class="iconfont">&#xe608;</i><span>下载客户端</span></a
           >
           <a href="javascript:;"
-            ><i class="iconfont">&#xe62d;</i><span>我 的</span></a
+            ><i class="iconfont">&#xe61d;</i><span>我 的</span></a
           >
         </div>
         <div class="toolver">
