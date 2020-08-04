dd17c65d38fca3fac3d6cddf9ceddff3f6eb05a3
81zhouxiaojia <1207638676@qq.com>
2020-08-04 12:23:43 +0800

    首页

---
+++ src/components/HeadSearch.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="search">
+    <div class="search_ipt">
+      <span>搜索:目的地/酒店/景点/航班点</span>
+    </div>
+    <a href="javascript:;" class="login_bt">我 的</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "head-search",
+};
+</script>
+
+<style lang="scss" scoped>
+.search {
+  height: 44px;
+  background-color: #fff;
+  padding-left: 12px;
+  display: flex;
+  align-items: center;
+  .search_ipt {
+    flex: 1;
+    position: relative;
+    span {
+      width: 100%;
+      display: block;
+      color: #666;
+      padding: 3px 5px;
+      padding-left: 35px;
+      overflow: hidden;
+      box-sizing: border-box;
+      border-radius: 15px;
+      background: rgba(236, 236, 237);
+    }
+    span::before {
+      position: absolute;
+      top: 0;
+      left: 0;
+      transform: translate(40%, 28%);
+      width: 35px;
+      height: 100%;
+      overflow: hidden;
+      background: url("../assets/xc-sprite.png") no-repeat;
+      background-size: 21px auto;
+      content: "";
+    }
+  }
+  .login_bt {
+    width: 51px;
+    height: 44px;
+    color: rgba(34, 34, 34);
+    text-align: center;
+    padding-right: 5px;
+    font-size: 13px;
+    display: flex;
+    flex-direction: column;
+    box-sizing: border-box;
+    box-sizing: -webkit-border-box;
+    &::before {
+      content: "";
+      margin: 0 auto;
+      width: 30px;
+      height: 30px;
+      background: url("../assets/xc-sprite.png") no-repeat;
+      background-size: 85%;
+      background-position: 1px -94px;
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="home">
+    <header class="header">
+      <head-search></head-search>
+    </header>
+    <!-- 头部 end -->
+    <div class="container">
+      <div class="banner-wrap">
+        <img src="../assets/baodi2@v7.15.jpg" alt="" />
+      </div>
+      <!-- banner end -->
+      <div class="local-nav">
+        <li>
+          <a href="javascript:;"><span>攻略.景点</span></a>
+        </li>
+        <li>
+          <a href="javascript:;"><span>门票.玩乐</span></a>
+        </li>
+        <li>
+          <a href="javascript:;"><span>美食林</span></a>
+        </li>
+        <li>
+          <a href="javascript:;"><span>周边游</span></a>
+        </li>
+        <li>
+          <a href="javascript:;"><span>一日游</span></a>
+        </li>
+      </div>
+      <!--本地导航 end -->
+      <nav>
+        <div class="nav-common">
+          <div class="nav-item item1"><span>酒店</span></div>
+          <div class="nav-item item2"><span>民宿.客栈</span></div>
+          <div class="nav-item item3"><span>特价.爆款</span></div>
+        </div>
+
+        <div class="nav-common">
+          <div class="nav-item item4"><span>机票</span></div>
+          <div class="nav-item item5"><span>火车票</span></div>
+          <div class="nav-item item6"><span>汽车.船票</span></div>
+          <div class="nav-item item7"><span>专车.租车</span></div>
+        </div>
+
+        <div class="nav-common">
+          <div class="nav-item item8"><span>旅游</span></div>
+          <div class="nav-item item9"><span>高铁游</span></div>
+          <div class="nav-item item10"><span>邮轮游</span></div>
+          <div class="nav-item item11"><span>定制游</span></div>
+        </div>
+      </nav>
+      <!--主导航 end -->
+      <ul class="main-nav">
+        <li>
+          <a href="javascript:;"><span class="main-nav-icon1">自由行</span></a>
+        </li>
+        <li>
+          <a href="javascript:;"
+            ><span class="main-nav-icon2">WiFi电话卡</span></a
+          >
+        </li>
+        <li>
+          <a href="javascript:;"
+            ><span class="main-nav-icon3">保险.签证</span></a
+          >
+        </li>
+        <li>
+          <a href="javascript:;"
+            ><span class="main-nav-icon4">换钞.签证</span></a
+          >
+        </li>
+        <li>
+          <a href="javascript:;"
+            ><span class="main-nav-icon5">当地向导</span></a
+          >
+        </li>
+        <li>
+          <a href="javascript:;"
+            ><span class="main-nav-icon6">特价机票</span></a
+          >
+        </li>
+        <li>
+          <a href="javascript:;"><span class="main-nav-icon7">礼品卡</span></a>
+        </li>
+        <li>
+          <a href="javascript:;"
+            ><span class="main-nav-icon8">电卡.借钱</span></a
+          >
+        </li>
+        <li>
+          <a href="javascript:;"><span class="main-nav-icon9">旅拍</span></a>
+        </li>
+        <li>
+          <a href="javascript:;"><span class="main-nav-icon10">更多</span></a>
+        </li>
+      </ul>
+      <!-- 自由行 end -->
+      <div class="hotAdvise">
+        <h2></h2>
+        <i class="advise"></i>
+        <a href="javascript:;"><span>获取更多福利 ></span></a>
+      </div>
+      <!-- 热门活动 end-->
+      <div class="row-adv">
+        <div></div>
+        <div></div>
+      </div>
+      <!-- 广告 end -->
+      <footer class="footer">
+        <div class="loadApp">
+          <a href="javascript:;"
+            ><i class="iconfont">&#xe60f;</i><span>电话预订</span></a
+          >
+          <a href="javascript:;"
+            ><i class="iconfont">&#xe608;</i><span>下载客户端</span></a
+          >
+          <a href="javascript:;"
+            ><i class="iconfont">&#xe62d;</i><span>我 的</span></a
+          >
+        </div>
+        <div class="toolver">
+          <a href="javascript:;">网站地图</a>
+          <a href="javascript:;">Language</a>
+          <a href="javascript:;">电脑版</a>
+        </div>
+        <div class="copyright">
+          &copy;2020携程旅行|备案号
+        </div>
+      </footer>
+      <!-- 底部链接 end -->
+    </div>
+  </div>
+</template>
+
+<script>
+import "@/assets/styles/index.scss";
+import HeadSearch from "../components/HeadSearch.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeadSearch,
+  },
+};
+</script>
